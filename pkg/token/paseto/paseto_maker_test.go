a35d90c6d3c365993cfe39796a2a290d86f66f71
thang.nn <thang.nn@atomsolution.com.vn>
2025-02-03 10:46:04 +0000

    feat: implement jwt+paseto authentication functionalities

---
+++ pkg/token/paseto/paseto_maker_test.go
@@ -0,0 +1,50 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ChokeGuy/simple-bank/util"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.UserName)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt.Time, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt.Time, time.Second)
+}
+
+func TestExpirePasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := -time.Minute
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, jwt.ErrTokenExpired.Error())
+	require.Nil(t, payload)
+}
