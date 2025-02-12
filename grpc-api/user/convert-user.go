eeebdcd7a8b22e6fcb0a033c8ec5b1eaac4c7e9a
thang.nn <thang.nn@atomsolution.com.vn>
2025-02-12 05:42:24 +0000

    feat:implement login+create user apis for grpc

---
+++ grpc-api/user/convert-user.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	db "github.com/ChokeGuy/simple-bank/db/sqlc"
+	"github.com/ChokeGuy/simple-bank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		UserName:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
